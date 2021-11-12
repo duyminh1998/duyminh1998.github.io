43c628c967067da4ce2206d5f7f5561a4f9d7486
Minh <minhhua12345@gmail.com>
2021-11-12 00:02:41 -0600

    add animation

--- demo.js
+++ demo.js
@@ -29,23 +29,25 @@ jQuery(function($){
                     if (leadingBit > 7) {
                         for (let i = 8; i <= leadingBit; i++) {
                             $("#input-bits-".concat(31 - i)).css("background-color", "#fc9b9b");
-                        };                        
+                        };
+                        $(".continue-container").html($("#continue-no-template").html());
+                        let inputNum = $(".val-input").val();
+                        if (inputNum) {
+                            App.setBits(inputNum, "#find-cor-bits-");
+                            let leadingBit = App.determineLeadBit(inputNum);
+                            if (leadingBit > 7) {
+                                for (let i = 8; i <= leadingBit; i++) {
+                                    $("#find-cor-bits-".concat(31 - i)).css("background-color", "#fc9b9b");
+                                };                        
+                            };
+                        };                                                
+                    } else {
+                        $(".continue-container").html($("#continue-yes-template").html());
                     };
                 };
             });
             // Continue
-            App.$doc.on('click', '#cont-no', function() {
-                $(".continue-container").html($("#continue-no-template").html());
-                let inputNum = $(".val-input").val();
-                if (inputNum) {
-                    App.setBits(inputNum, "#find-cor-bits-");
-                    let leadingBit = App.determineLeadBit(inputNum);
-                    if (leadingBit > 7) {
-                        for (let i = 8; i <= leadingBit; i++) {
-                            $("#find-cor-bits-".concat(31 - i)).css("background-color", "#fc9b9b");
-                        };                        
-                    };
-                };                
+            App.$doc.on('click', '#cont-no', function() {               
             });
             // First right arrow  
             App.$doc.on('click', '#find-cor-bits-right-arrow', function() {
@@ -71,7 +73,8 @@ jQuery(function($){
                     App.rotAmt = 0;
                     App.timesClickedShowOGBits = 0;
                     App.setBits(bitRotated, "#rot-back-bits-");                      
-                };                
+                    $("#8-bit-equals").text(bitRotated);
+                };               
             });
             // First left arrow  
             App.$doc.on('click', '#find-cor-bits-left-arrow', function() {
@@ -96,8 +99,9 @@ jQuery(function($){
                     $(".continue-to-rot-container").html($("#continue-to-rot-template").html());
                     App.rotAmt = 0;
                     App.timesClickedShowOGBits = 0;
-                    App.setBits(bitRotated, "#rot-back-bits-");               
-                };                
+                    App.setBits(bitRotated, "#rot-back-bits-");
+                    $("#8-bit-equals").text(bitRotated);               
+                };                 
             });
             // Show original input bits
             App.$doc.on('click', '#show-og-bits-btn', function() {
@@ -113,6 +117,7 @@ jQuery(function($){
             // Second right arrow
             App.$doc.on('click', '#rot-back-right-arrow', function() {
                 let bits = App.getBits("#rot-back-bits-");
+                let ogBits = App.getBits("#find-cor-bits-");
                 let bitRotated = App.ror(bits, 2);
                 App.setBits(bitRotated, "#rot-back-bits-");
                 App.rotAmt = (App.rotAmt + 2) % 32;
@@ -121,13 +126,25 @@ jQuery(function($){
                     $("#correct-answer").text("You have lined up the 8 bits into the original bits. The correct ARM immediate encoding for ".concat(
                         $(".val-input").val(),
                         " is ",
-                        App.getBits("#find-cor-bits-"),
+                        ogBits,
                         " ROR ",
                         App.rotAmt,
                         "."
                     ));
+                    $('#animation-container').html($('#play-animation-template').html());
+                    $("#pre-animation-blurb").text("Here is a visualization of how ARM reconstructs the original number of ".concat(
+                        $(".val-input").val(),
+                        " by rotating ",
+                        ogBits,
+                        " ",
+                        App.rotAmt,
+                        " times to the right. Press play animation to check it out."
+                    )); 
+                    App.setBits(ogBits, "#animation-bits-");
+                    $("#animation-blurb").text(String(ogBits).concat(" ROR 0 = ", ogBits));                    
                 } else {
                     $("#correct-answer").text('');
+                    $('#animation-container').html('');
                 };
             });
             // Reset button click
@@ -136,7 +153,22 @@ jQuery(function($){
                 App.rotAmt = 0;
                 $("#rot-amt-blurb").text("You have rotated the original value ".concat(App.rotAmt, " times to the right."));   
                 $("#correct-answer").text('');             
-            });            
+            });
+            // Play animation
+            App.$doc.on('click', '#play-animation-btn', async function() {
+                let bitRotated = App.getBits("#find-cor-bits-");
+                let ogBits = bitRotated;
+                App.setBits(bitRotated, "#animation-bits-");
+                $("#animation-blurb").text(String(ogBits).concat(" ROR ", 0," = ", ogBits));
+                let curRotAmt = 2;
+                while (curRotAmt <= App.rotAmt) {
+                    await App.sleep(1000);
+                    bitRotated = App.ror(bitRotated, 2);
+                    App.setBits(bitRotated, "#animation-bits-");
+                    $("#animation-blurb").text(String(ogBits).concat(" ROR ", curRotAmt," = ", bitRotated));
+                    curRotAmt = curRotAmt + 2;
+                }
+            });                        
             // Prevent enter button from refreshing page
             App.$doc.on('keyup keypress', function(e) {
                 var keyCode = e.keyCode || e.which;
@@ -145,6 +177,10 @@ jQuery(function($){
                     return false;
                 }
             });            
+        },
+
+        sleep: function(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
         },
 
         rol: function(n, i) {
